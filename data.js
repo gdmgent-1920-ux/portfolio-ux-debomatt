1d4edf681587cc05f6bd75185a7a0878a480a56c
debomatt <61147950+debomatt@users.noreply.github.com>
2020-02-28 21:01:53 +0100

    share link updaten

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/f1fa2da0-2b5c-497e-7c13-b2114624b2a2-87cb/",
                 "target": "_blank"
             }
         ]
